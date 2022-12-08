665adcf4f64556c337c4c00baf32bb11dfd815b3
Arthur <el20a2V@leeds.ac.uk>
2022-12-08 16:05:29 +0000

    Simplified GPIO names

--- GPIO.h
+++ GPIO.h
@@ -5,45 +5,45 @@
 
 struct GPIO_registers
 {
-    volatile uint32_t GPIODATA;
+    volatile uint32_t DATA;
     volatile const uint32_t PADDING0[0xFF];
-    volatile uint32_t GPIODIR;
-    volatile uint32_t GPIOIS;
-    volatile uint32_t GPIOIBE;
-    volatile uint32_t GPIOIEV;
-    volatile uint32_t GPIOIM;
-    volatile uint32_t GPIORIS;
-    volatile uint32_t GPIOMIS;
-    volatile uint32_t GPIOICR;
-    volatile uint32_t GPIOAFSEL;
+    volatile uint32_t DIR;
+    volatile uint32_t IS;
+    volatile uint32_t IBE;
+    volatile uint32_t IEV;
+    volatile uint32_t IM;
+    volatile uint32_t RIS;
+    volatile uint32_t MIS;
+    volatile uint32_t ICR;
+    volatile uint32_t AFSEL;
     volatile const uint32_t PADDING1[0x37];
-    volatile uint32_t GPIODR2R;
-    volatile uint32_t GPIODR4R;
-    volatile uint32_t GPIODR8R;
-    volatile uint32_t GPIOODR;
-    volatile uint32_t GPIOPUR;
-    volatile uint32_t GPIOPDR;
-    volatile uint32_t GPIOSLR;
-    volatile uint32_t GPIODEN;
-    volatile uint32_t GPIOLOCK;
-    volatile uint32_t GPIOCR;
-    volatile uint32_t GPIOAMSEL;
-    volatile uint32_t GPIOPCTL;
-    volatile uint32_t GPIOADCCTL;
-    volatile uint32_t GPIODMACTL;
+    volatile uint32_t DR2R;
+    volatile uint32_t DR4R;
+    volatile uint32_t DR8R;
+    volatile uint32_t ODR;
+    volatile uint32_t PUR;
+    volatile uint32_t PDR;
+    volatile uint32_t SLR;
+    volatile uint32_t DEN;
+    volatile uint32_t LOCK;
+    volatile uint32_t CR;
+    volatile uint32_t AMSEL;
+    volatile uint32_t PCTL;
+    volatile uint32_t ADCCTL;
+    volatile uint32_t DMACTL;
     volatile const uint32_t PADDING2[0x2A6];
-    volatile uint32_t GPIOPID4;
-    volatile uint32_t GPIOPID5;
-    volatile uint32_t GPIOPID6;
-    volatile uint32_t GPIOPID7;
-    volatile uint32_t GPIOPID0;
-    volatile uint32_t GPIOPID1;
-    volatile uint32_t GPIOPID2;
-    volatile uint32_t GPIOPID3;
-    volatile uint32_t GPIOPCID0;
-    volatile uint32_t GPIOPCID1;
-    volatile uint32_t GPIOPCID2;
-    volatile uint32_t GPIOPCID3;
+    volatile uint32_t PID4;
+    volatile uint32_t PID5;
+    volatile uint32_t PID6;
+    volatile uint32_t PID7;
+    volatile uint32_t PID0;
+    volatile uint32_t PID1;
+    volatile uint32_t PID2;
+    volatile uint32_t PID3;
+    volatile uint32_t PCID0;
+    volatile uint32_t PCID1;
+    volatile uint32_t PCID2;
+    volatile uint32_t PCID3;
 };
 
 #define PORTA_BASE_APB  0x40004000
@@ -59,11 +59,11 @@ struct GPIO_registers
 #define PORTF_BASE_APB  0x40025000
 #define PORTF_BASE_AHB  0x4005D000
 
-#define GPIO_PORTA  ((struct GPIO_registers *)PORTA_BASE_APB)
-#define GPIO_PORTB  ((struct GPIO_registers *)PORTB_BASE_APB)
-#define GPIO_PORTC  ((struct GPIO_registers *)PORTC_BASE_APB)
-#define GPIO_PORTD  ((struct GPIO_registers *)PORTD_BASE_APB)
-#define GPIO_PORTE  ((struct GPIO_registers *)PORTE_BASE_APB)
-#define GPIO_PORTF  ((struct GPIO_registers *)PORTF_BASE_APB)
+#define PORTA  ((struct GPIO_registers *)PORTA_BASE_APB)
+#define PORTB  ((struct GPIO_registers *)PORTB_BASE_APB)
+#define PORTC  ((struct GPIO_registers *)PORTC_BASE_APB)
+#define PORTD  ((struct GPIO_registers *)PORTD_BASE_APB)
+#define PORTE  ((struct GPIO_registers *)PORTE_BASE_APB)
+#define PORTF  ((struct GPIO_registers *)PORTF_BASE_APB)
 
 #endif
