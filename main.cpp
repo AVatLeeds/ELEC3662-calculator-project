12f8de0eb9e2248e8742c1f6b69042738a59c114
Arthur <el20a2V@leeds.ac.uk>
2022-12-15 13:03:24 +0000

    Main now tests keypad state.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <stdint.h>
 #include "status.h"
+#include "LCD_driver.h"
+#include "keypad.h"
 
 status_LED status;
+LCD_driver LCD;
+Keypad keypad;
+
+void keypad_state_test()
+{
+    LCD.cursor_pos(1, 0);
+    uint32_t keypress = keypad.value();
+    LCD.print("Keypad: 0x");
+    LCD.putchar(((keypress <<= 4), ((keypress & 0xF0000) >> 16) + 0x30 + ((keypress & 0xF0000) > 0x90000) * 0x7));
+    LCD.putchar(((keypress <<= 4), ((keypress & 0xF0000) >> 16) + 0x30 + ((keypress & 0xF0000) > 0x90000) * 0x7));
+    LCD.putchar(((keypress <<= 4), ((keypress & 0xF0000) >> 16) + 0x30 + ((keypress & 0xF0000) > 0x90000) * 0x7));
+    LCD.putchar(((keypress <<= 4), ((keypress & 0xF0000) >> 16) + 0x30 + ((keypress & 0xF0000) > 0x90000) * 0x7));
+}
 
 int main(void)
 {   
     status = WHITE;
-    while(1);
+    LCD.print("Hello Oti :)");
+    uint8_t pressed = 0;
+
+    while (1)
+    {
+        keypad_state_test();
+    }
 }
 
 void hard_fault()
