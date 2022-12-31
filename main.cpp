2c5c305e895bc312c5c2393d9b23fccffd90990e
Arthur <el20a2V@leeds.ac.uk>
2022-12-31 00:37:02 +0000

    main now handles keypresses and instantiates calculator.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "calculator.h"
 #include "status.h"
 #include "LCD_driver.h"
 #include "keypad.h"
@@ -6,6 +7,7 @@
 status_LED status;
 LCD_driver LCD;
 Keypad keypad;
+Calculator calc;
 
 void keypad_state_test()
 {
@@ -21,12 +23,51 @@ void keypad_state_test()
 int main(void)
 {   
     status = WHITE;
-    LCD.print("Hello Oti :)");
+    //LCD.print("Hello Oti :)");
     uint8_t pressed = 0;
 
     while (1)
     {
-        keypad_state_test();
+        //keypad_state_test();
+        uint32_t keypress = keypad.value();
+        switch (keypress)
+        {
+            case 0x8000: pressed ? : (calc.buffer_insert('1'), pressed = 1); break;
+            case 0x4000: pressed ? : (calc.buffer_insert('2'), pressed = 1); break;
+            case 0x2000: pressed ? : (calc.buffer_insert('3'), pressed = 1); break;
+            case 0x0800: pressed ? : (calc.buffer_insert('4'), pressed = 1); break;
+            case 0x0400: pressed ? : (calc.buffer_insert('5'), pressed = 1); break;
+            case 0x0200: pressed ? : (calc.buffer_insert('6'), pressed = 1); break;
+            case 0x0080: pressed ? : (calc.buffer_insert('7'), pressed = 1); break;
+            case 0x0040: pressed ? : (calc.buffer_insert('8'), pressed = 1); break;
+            case 0x0020: pressed ? : (calc.buffer_insert('9'), pressed = 1); break;
+            case 0x0008: pressed ? : (calc.buffer_insert('.'), pressed = 1); break;
+            case 0x0004: pressed ? : (calc.buffer_insert('0'), pressed = 1); break;
+            case 0x0002: pressed ? : (calc.buffer_insert('E'), pressed = 1); break;
+
+            case 0x9000: pressed ? : (calc.buffer_insert('^'), pressed = 1); break;
+            case 0x5000: pressed ? : (calc.buffer_insert('*'), pressed = 1); break;
+            case 0x3000: pressed ? : (calc.buffer_insert('+'), pressed = 1); break;
+            case 0x1800: pressed ? : (calc.buffer_insert(0xE8), pressed = 1); break;
+            case 0x1400: pressed ? : (calc.buffer_insert(0xFD), pressed = 1); break;
+            case 0x1200: pressed ? : (calc.buffer_insert('-'), pressed = 1); break;
+            case 0x1080: pressed ? : (calc.buffer_insert('('), pressed = 1); break;
+            case 0x1020: pressed ? : (calc.buffer_insert(')'), pressed = 1); break;
+
+            case 0x1040: pressed ? : (calc.buffer_insert_text("Hello Oti"), pressed = 1); break;
+
+            case 0x0100: pressed ? : (calc.toggle_sign(), pressed = 1); break;
+            case 0x0010: pressed ? : (calc.buffer_backspace(), pressed = 1); break;  
+            case 0x0001: pressed ? : (calc.evaluate(), pressed = 1); break;
+
+            case 0x1008: pressed ? : (calc.cursor_left(), pressed = 1); break;
+            case 0x1002: pressed ? : (calc.cursor_right(), pressed = 1); break;
+            
+            case 0x1000:
+            case 0x0000: pressed = 0; break;
+
+            default: break;             
+        }
     }
 }
 
